38cc18108b5ed48f7394939529704eb361d974d4
sudheer-akkio <sudheer@akkio.com>
2025-11-11 22:18:29 -0800

    added skeleton for the SQL models

---
+++ models/locality/locality_campaign_exposure.sql
@@ -0,0 +1,16 @@
+
+-- Use the `ref` function to select from other models
+
+{{ config(materialized='table') }}
+
+with source_data as (
+
+    select 1 as id
+    union all
+    select null as id
+
+)
+
+select *
+from source_data
+
