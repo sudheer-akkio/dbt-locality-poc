4d47fafcc36fdeac0feec4914c33c0625c34bb23
connor-akkio <connor@akkio.com>
2026-03-12 15:49:28 -0700

    gonna figure this out

--- macros/locality_id_col.sql
+++ macros/locality_id_col.sql
@@ -1,7 +1,7 @@
 {%- macro locality_id_col() -%}
-{%- if target.name == 'staging' -%}AKKIO_ID{%- else -%}LOCALITY_ID{%- endif -%}
+{%- if var('env') == 'staging' -%}AKKIO_ID{%- else -%}LOCALITY_ID{%- endif -%}
 {%- endmacro -%}
 
 {%- macro locality_hh_id_col() -%}
-{%- if target.name == 'staging' -%}AKKIO_HH_ID{%- else -%}LOCALITY_HH_ID{%- endif -%}
+{%- if var('env') == 'staging' -%}AKKIO_HH_ID{%- else -%}LOCALITY_HH_ID{%- endif -%}
 {%- endmacro -%}
