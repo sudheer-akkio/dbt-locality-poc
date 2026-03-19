f4b20a0c07a210096dc04610a6d3049740a3a71a
Alec Cumming <alec@akkio.com>
2026-03-18 23:46:35 -0400

    fixed var mismatch when running --target prod without env vars

--- macros/locality_id_col.sql
+++ macros/locality_id_col.sql
@@ -1,21 +1,7 @@
-{%- macro get_env() -%}
-{#- Resolution order:
-    1. --vars '{"env":"prod"}'  (Databricks job params / explicit override)
-    2. target.name              (local: dbt run --target prod)
-    Default: staging -#}
-{%- if var('env', 'staging') != 'staging' -%}
-  {{- var('env') -}}
-{%- elif target.name == 'prod' -%}
-  {{- 'prod' -}}
-{%- else -%}
-  {{- 'staging' -}}
-{%- endif -%}
-{%- endmacro -%}
-
 {%- macro locality_id_col() -%}
-{%- if get_env() == 'staging' -%}AKKIO_ID{%- else -%}LOCALITY_ID{%- endif -%}
+{%- if var('env', 'staging') == 'staging' -%}AKKIO_ID{%- else -%}LOCALITY_ID{%- endif -%}
 {%- endmacro -%}
 
 {%- macro locality_hh_id_col() -%}
-{%- if get_env() == 'staging' -%}AKKIO_HH_ID{%- else -%}LOCALITY_HH_ID{%- endif -%}
+{%- if var('env', 'staging') == 'staging' -%}AKKIO_HH_ID{%- else -%}LOCALITY_HH_ID{%- endif -%}
 {%- endmacro -%}
